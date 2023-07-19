cc7a8b5635f4c82fb759d1e3eab6c65079517976
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-19 17:08:17 -0500

    agregando comentarios

--- Trie.java
+++ Trie.java
@@ -1,44 +1,56 @@
-import java.util.*;
 
 class Trie<T> {
     private Node<T> root;
 
     public Trie() {
-        this.root = new Node<>(null);
+        this.root = new Node<>(null); // Se crea el nodo raíz del Trie sin valor asociado
     }
 
+    // Método para insertar una palabra en el Trie
     public void insert(String word) {
-        Node<T> currentNode = root;
+        Node<T> currentNode = root; // Se comienza desde el nodo raíz
+
+        // Se recorre cada carácter de la palabra
         for (char character : word.toCharArray()) {
-            T data = (T) Character.valueOf(character);
-            Node<T> child = findChild(currentNode, data);
+            T data = (T) Character.valueOf(character); // Se convierte el carácter en el tipo genérico T
+            Node<T> child = findChild(currentNode, data); // Se busca un hijo con el carácter actual
+
             if (child == null) {
-                child = new Node<>(data);
-                currentNode.addChild(child);
+                child = new Node<>(data); // Si no existe un hijo con el carácter, se crea uno nuevo
+                currentNode.addChild(child); // Se agrega el nuevo hijo al nodo actual
             }
-            currentNode = child;
+
+            currentNode = child; // Se actualiza el nodo actual al hijo correspondiente
         }
     }
 
+    // Método para buscar una palabra en el Trie
     public boolean search(String word) {
-        Node<T> currentNode = root;
+        Node<T> currentNode = root; // Se comienza desde el nodo raíz
+
+        // Se recorre cada carácter de la palabra
         for (char character : word.toCharArray()) {
-            T data = (T) Character.valueOf(character);
-            Node<T> child = findChild(currentNode, data);
+            T data = (T) Character.valueOf(character); // Se convierte el carácter en el tipo genérico T
+            Node<T> child = findChild(currentNode, data); // Se busca un hijo con el carácter actual
+
             if (child == null) {
-                return false;
+                return false; // Si no se encuentra un hijo con el carácter, la palabra no está en el Trie
             }
-            currentNode = child;
+
+            currentNode = child; // Se actualiza el nodo actual al hijo correspondiente
         }
-        return true;
+
+        return true; // Se encontraron todos los caracteres, la palabra está en el Trie
     }
 
+    // Método auxiliar para buscar un hijo con un valor específico en un nodo
     private Node<T> findChild(Node<T> node, T data) {
         for (Node<T> child : node.getChildren()) {
             if (child.getData().equals(data)) {
-                return child;
+                return child; // Se encontró un hijo con el valor específico
             }
         }
-        return null;
+
+        return null; // No se encontró un hijo con el valor específico
     }
 }
