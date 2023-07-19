005d84b1fc1d500b4bad60d5294b11a46f329558
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-07-19 17:25:49 -0500

    comentarios

--- avanceFinalAndreDelgado/Main.java
+++ avanceFinalAndreDelgado/Main.java
@@ -4,13 +4,13 @@ public class Main {
         Trie<String> trie = new Trie<>();
 
         // Insertar palabras en el Trie
-        trie.insert("apple");
+        trie.insert("manzana");
         trie.insert("banana");
-        trie.insert("cat");
+        trie.insert("gato");
 
         // Realizar búsquedas en el Trie
-        System.out.println(trie.search("apple")); // Debería imprimir: true
+        System.out.println(trie.search("manzana")); // Debería imprimir: true
         System.out.println(trie.search("banana")); // Debería imprimir: true
-        System.out.println(trie.search("dog")); // Debería imprimir: false
+        System.out.println(trie.search("perro")); // Debería imprimir: false
     }
 }
