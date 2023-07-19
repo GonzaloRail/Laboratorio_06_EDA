47f639f6af6c4fc89a50cc3d4c2bcbf9fbae08b3
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-07-19 17:18:32 -0500

    clase main haciendo una pequeña prueba

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Trie<String> trie = new Trie<>();
+
+        // Insertar palabras en el Trie
+        trie.insert("apple");
+        trie.insert("banana");
+        trie.insert("cat");
+
+        // Realizar búsquedas en el Trie
+        System.out.println(trie.search("apple")); // Debería imprimir: true
+        System.out.println(trie.search("banana")); // Debería imprimir: true
+        System.out.println(trie.search("dog")); // Debería imprimir: false
+    }
+}
