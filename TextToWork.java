8639c75147f5a8db25c29bf638dadbc10c60e7f6
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-07-25 03:24:08 -0500

    Prototipo en bruto del gui para insertar texto

--- TextToWork.java
+++ TextToWork.java
@@ -8,7 +8,7 @@ import java.awt.FlowLayout;
 
 public class TextToWork extends JFrame {
     private final int ancho = 700;
-    private final int largo = 500;
+    private final int largo = 400;
     
     private final FindAndReplace operations = new FindAndReplace();
     
@@ -17,23 +17,39 @@ public class TextToWork extends JFrame {
         this.setTitle("Find and Replace");
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout(5, 1));
+        this.setLayout(new GridLayout(3, 1));
         build();
         setVisible(true);
     }
     
     private void build() {
         
+        JPanel textPanel = new JPanel(new BorderLayout());
+        JLabel indication = new JLabel("Inserte su texto aqui:");
         JTextArea text = new JTextArea();
         text.setLineWrap(true); 
         text.setWrapStyleWord(true);
         
         JScrollPane textArea = new JScrollPane(text);
         
+        textPanel.add(indication, BorderLayout.NORTH);
+        textPanel.add(textArea, BorderLayout.CENTER);
+        
         textArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         
-        add(textArea);
+        int topSpace = 5;
+        int leftSpace = 5;
+        int bottomSpace = 5;
+        int rightSpace = 5;
+        EmptyBorder border = new EmptyBorder(topSpace, leftSpace, bottomSpace, rightSpace);
         
+        indication.setBorder(border);
+        textArea.setBorder(border);
+        
+        add(textPanel);
+        
+        JPanel textViewPanel = new JPanel(new BorderLayout());
+        JLabel indication2 = new JLabel("Su texto:");
         JTextArea textView = new JTextArea();
         textView.setLineWrap(true); 
         textView.setWrapStyleWord(true);
@@ -43,7 +59,13 @@ public class TextToWork extends JFrame {
         
         textViewArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         
-        add(textViewArea);
+        textViewPanel.add(indication2, BorderLayout.NORTH);
+        textViewPanel.add(textViewArea, BorderLayout.CENTER);
+        
+        indication2.setBorder(border);
+        textViewArea.setBorder(border);
+        
+        add(textViewPanel);
         
         JPanel buttons = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JButton showOptions = new JButton("Buscar y Reemplazar");
