ac688cb46a013c9808f02fb16bcfdc2023cf648f
André Hilacondo <116049544+ahilacondo@users.noreply.github.com>
2023-07-26 05:12:28 +0000

    Eliminamos archivos innecesarios

--- design.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-interface design {
-	Font buttonFont = new Font("Arial", Font.PLAIN, 14);
-	Color buttonColor = new Color(196,196,196);
-    Color buttonColorClick = new Color(192,230,49);
-    Color buttonColorDeactivate = new Color(128, 128,128);
-}
-
